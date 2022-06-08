f2f79390bcaab8f16d51515f7adfb623385b5ddb
EdCope <ed321@hotmail.co.uk>
2022-06-08 20:58:49 +0100

    Refactor

--- Python/lasagna/lasagna.py
+++ Python/lasagna/lasagna.py
@@ -1,8 +1,7 @@
+from re import A
+
+
 EXPECTED_BAKE_TIME = 40
 
 def bake_time_remaining(actual_time):
-    if(actual_time == 30):
-        return 10
-    elif(actual_time == 35):
-        return 5
-    return 20
+    return EXPECTED_BAKE_TIME - actual_time
