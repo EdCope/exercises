a413e9d281256e9d94f92297d8d35b33cd2a5a2b
EdCope <ed321@hotmail.co.uk>
2022-06-08 20:52:30 +0100

    Hardcoded amount

--- Python/lasagna/lasagna.py
+++ Python/lasagna/lasagna.py
@@ -1,1 +1,4 @@
 EXPECTED_BAKE_TIME = 40
+
+def bake_time_remaining(actual_time):
+    return 20
--- Python/lasagna/lasagna_test.py
+++ Python/lasagna/lasagna_test.py
@@ -2,3 +2,7 @@ from lasagna import *
 
 def test_EXPECTED_BAKE_TIME():
     assert EXPECTED_BAKE_TIME == 40
+
+def test_bake_time_remaining():
+    actualTimeRemaining = 20
+    assert bake_time_remaining(actualTimeRemaining) == 20
